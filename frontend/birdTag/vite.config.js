42842f081a830dade54b8ef33e2046bbde62ebc4
Yao <zhangyao.jk@gmail.com>
2025-06-04 14:30:40 +1000

    Completed Login Function

--- frontend/birdTag/vite.config.js
+++ frontend/birdTag/vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+      process: 'process',
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    include: ['buffer', 'process'],
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
+  },
+  server: {
+    proxy: {
+      '/bird': {
+        target: 'https://voe9xiqt31.execute-api.us-east-1.amazonaws.com/version1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/bird/, ''),
+      },
+    },
+  },
+});
